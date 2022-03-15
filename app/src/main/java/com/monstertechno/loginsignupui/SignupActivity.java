7066d60ff1ad570e81217f393c23fb6566bd956c
unknown <SRPking1>
2022-03-16 02:07:48 +0530

    otp view

--- app/src/main/java/com/monstertechno/loginsignupui/SignupActivity.java
+++ app/src/main/java/com/monstertechno/loginsignupui/SignupActivity.java
@@ -17,22 +17,22 @@ public class SignupActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_main);
 
 
-        TextView welcm_txt = (TextView) findViewById(R.id.wlcm_indilive);
-        TextView quickLoginMobile_txt = (TextView) findViewById(R.id.quick_login_mobile);
-        TextView phoneNumberTxt = (TextView) findViewById(R.id.phn_num_txt);
-        TextView otherQuickSigin = (TextView) findViewById(R.id.otherQuickSignin);
+//        TextView welcm_txt = (TextView) findViewById(R.id.wlcm_indilive);
+//        TextView quickLoginMobile_txt = (TextView) findViewById(R.id.quick_login_mobile);
+//        TextView phoneNumberTxt = (TextView) findViewById(R.id.phn_num_txt);
+//        TextView otherQuickSigin = (TextView) findViewById(R.id.otherQuickSignin);
 
-        Typeface face_bold_oswald = ResourcesCompat.getFont(this,R.font.oswald_regular);
+ //       Typeface face_bold_oswald = ResourcesCompat.getFont(this,R.font.oswald_regular);
 //        Typeface face_normal_caviar = ResourcesCompat.getFont(this,R.font.caviar_dreams);
 //        Typeface face_bold_caviar = ResourcesCompat.getFont(this,R.font.caviar_dreams_bold);
 
-        welcm_txt.setTypeface(face_bold_oswald);
-        quickLoginMobile_txt.setTypeface(face_bold_oswald);
-        phoneNumberTxt.setTypeface(face_bold_oswald);
-        otherQuickSigin.setTypeface(face_bold_oswald);
+//        welcm_txt.setTypeface(face_bold_oswald);
+//        quickLoginMobile_txt.setTypeface(face_bold_oswald);
+//        phoneNumberTxt.setTypeface(face_bold_oswald);
+//        otherQuickSigin.setTypeface(face_bold_oswald);
 
 
     }
