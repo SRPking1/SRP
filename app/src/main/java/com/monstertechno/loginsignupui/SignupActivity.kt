e59823d77ead09d9f3049db964dddbfe47e5561f
unknown <SRPking1>
2022-04-01 02:15:23 +0530

    profile_register

--- app/src/main/java/com/monstertechno/loginsignupui/SignupActivity.kt
+++ app/src/main/java/com/monstertechno/loginsignupui/SignupActivity.kt
@@ -1,18 +1,42 @@
 package com.monstertechno.loginsignupui
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
-import android.view.animation.AnimationUtils
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatImageView
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class SignupActivity : AppCompatActivity() {
+    val myCalendar = Calendar.getInstance()
+    var editText: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_user_detail_info_info)
+        setContentView(R.layout.profile_detail_register)
+        editText = findViewById<View>(R.id.dob) as EditText
+        val date =
+            OnDateSetListener { view, year, month, day ->
+                myCalendar.set(Calendar.YEAR, year)
+                myCalendar.set(Calendar.MONTH, month)
+                myCalendar.set(Calendar.DAY_OF_MONTH, day)
+                updateLabel()
+            }
+        editText!!.setOnClickListener(View.OnClickListener {
+            DatePickerDialog(
+                this@SignupActivity,
+                date,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        })
+    }
+    private fun updateLabel() {
+        val myFormat = "MM/dd/yy"
+        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        editText?.setText(dateFormat.format(myCalendar.time))
     }
 }
